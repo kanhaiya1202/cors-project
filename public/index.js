caba2f4b06235519fe5c25b092610cdf968f5d65
kanhaiya sharma <149193709+kanhaiya1202@users.noreply.github.com>
2025-03-20 12:10:16 +0530

    Update index.js
    change code index.js

--- public/index.js
+++ public/index.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
+// assignment
+//1.create a backend server in node.js that return the sum endpoint
+//2.write an html file that hit the backend server using the fetch api
 
-app.use(express.json());
+const express = require("express");
+const { dirname } = require("path");
+const cors = require('cors');
+const app = express();
+
+
+app.use(express.json())
 app.use(cors());
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname+ "cors-project/public/index.html")
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 
 app.post("/sum", function(req, res){
     const a = parseInt(req.body.a);
     const b = parseInt(req.body.b);
+    console.log(a+b);
 
     res.json({
-        answer:a+b
+        ans:a+b
     });
+    
+
 });
 
 app.listen(3000);
